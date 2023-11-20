fe96730428f305809474a8f2766dd4cf17e5c101
user1 <user@domain.invalid>
2023-11-20 22:21:06 +0000

    build!: remove install.sh and rely on user linking

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "$(realpath "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)
-
-
-ln -s "$SCRIPT_DIR"/upkg "${XDG_BIN_HOME:-$HOME/.local/bin}"/upkg
